9246caf5b63f3177b6fd431077ee0f7cf240714a
anibalcastro <anibalcastro1515@gmail.com>
2022-07-05 14:36:41 -0600

    comments

--- CRUD + HTTP Error/src/app.js
+++ CRUD + HTTP Error/src/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const HttpError = require("./models/http-error");
 const mongoose = require("mongoose");
-const app = express();
+
+const HttpError = require("./models/http-error");
 const catsRouter = require("./routes/cats.router");
 const usersRouter = require("./routes/users.router");
-const port = 3010;
-//URL que se obtiene de mongoDB (atlas)
-const url =
-"mongodb+srv://anibalcastro:Web02@cluster0.e82klf0.mongodb.net/CatsDB?retryWrites=true&w=majority";
-const cors = require('cors');
+
+
+const app = express(); // Uso de express en app
+const port = 3010; //Puerto determinado
+const url = "mongodb+srv://anibalcastro:Web02@cluster0.e82klf0.mongodb.net/CatsDB?retryWrites=true&w=majority"; //URL que se obtiene de mongoDB (atlas)
+const cors = require('cors'); //Uso de cors
 
 
 app.use(cors()); //Lo use todo el mundo
